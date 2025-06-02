4e328c174bbd6fc398fcdb0d4ed501b0de8c676e
Kai <jauregiazcue@gmail.com>
2025-06-02 16:53:00 +0200

    change error handle to be shorter, change pwd regex to need a lower case, an upper case, a number and a symbol

--- server/src/controllers/user/userController.ts
+++ server/src/controllers/user/userController.ts
@@ -11,36 +11,23 @@ import {
 import { userInterface } from "../../models/user.ts";
 
 
-async function register(userData : userInterface) {
-    if (!userData.name) {
-        throw new UserNameNotProvided();
-    }
+async function register(userData: userInterface) {
+    if (!userData.name) throw new UserNameNotProvided();
+    if (!userData.email) throw new UserEmailNotProvided();
 
-    if (!userData.email) {
-        throw new UserEmailNotProvided();
-    }
+    const pwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).{8,}$/;
 
-    const pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!pwdRegex.test(userData.password)) throw new UserInvalidCredentials();
+    if (!userData.password) throw new UserPasswordNotProvided();
 
-    if (!pwdRegex.test(userData.password)) {
-        throw new UserInvalidCredentials();
-    }
-
-    if (!userData.password) {
-        throw new UserPasswordNotProvided();
-    }
 
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-    if (!emailRegex.test(userData.email)) {
-        throw new UserInvalidCredentials();
-    }
+    if (!emailRegex.test(userData.email)) throw new UserInvalidCredentials();
 
-    const oldUser = await User.findOne({email: userData.email});
+    const oldUser = await User.findOne({ email: userData.email });
 
-    if (oldUser) {
-        throw new UserEmailAlreadyExists();
-    }
+    if (oldUser) throw new UserEmailAlreadyExists();
 
     const hashedPassword = await hash(userData.password);
 
@@ -51,17 +38,12 @@ async function register(userData : userInterface) {
     return newUser;
 }
 
-async function login(email : string, password : string) {
+async function login(email: string, password: string) {
 
-    if (!email) {
-        throw new UserEmailNotProvided();
-    }
+    if (!email) throw new UserEmailNotProvided();
+    if (!password) throw new UserPasswordNotProvided();
 
-    if (!password) {
-        throw new UserPasswordNotProvided();
-    }
-
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
     if (!user) throw new UserInvalidCredentials();
 
