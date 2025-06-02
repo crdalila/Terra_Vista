9ec7d688d847337825e82f764487f5ec28e1ccfb
Kai <jauregiazcue@gmail.com>
2025-06-02 16:44:09 +0200

    deleted ping pong route

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,10 +9,6 @@ app.use(express.json());
 
 app.use("/clickup", clickUpRoutes);
 
-app.get("/ping", (req, res) => {
-	res.send("pong");
-});
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
