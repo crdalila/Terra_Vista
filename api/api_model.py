34f5a1b493cf9a4add6994cfb6682a819cc77117
ItsazainBilbao <itsazainmbilbao@gmail.com>
2025-06-03 16:28:47 +0200

    3 endpoints with dummie args

--- api/api_model.py
+++ api/api_model.py
@@ -14,6 +14,10 @@ root_path = "/api/api_model.py"
 
 @app.route('/v1/predictCategory', methods=['GET'])
 def predict():
+    request_text = request.args.get('request_text')
+
+    if None in request_text:
+        return jsonify({"error": "No parameters"}), 400
 
     catDict = { 0 : "Copy Revision", 1 : "Design Issues", 2 : "Requested Change", 3 : "New Item"}
     rndNumber = np.random.randint(0,4)
@@ -23,9 +27,23 @@ def predict():
 
 @app.route('/v1/predictPriority', methods=['GET'])
 def predict():
+    request_text = request.args.get('request_text')
 
-    prioDict = { 0 : "Low", 1 : "Medium", 2 : "High"}
+    if None in request_text:
+        return jsonify({"error": "No parameters"}), 400
+
+    prioDict = { 0 : "Low", 1 : "Medium", 2 : "High", 3: "Urgent"}
     rndNumber = np.random.randint(0,3)
 
     prediction = {"Prioridad": prioDict[rndNumber]}
+    return jsonify({'predictions': prediction[0]})
+
+@app.route('/v1/predicDuration', methods=['GET'])
+def predict():
+    request_text = request.args.get('request_text')
+
+    if None in request_text:
+        return jsonify({"error": "No parameters"}), 400
+
+    prediction = {"Duration": np.random.choice([1, 2, 3, 5, 8, 13])}
     return jsonify({'predictions': prediction[0]})
