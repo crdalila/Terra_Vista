5dc071a690281b241dc2cb8b918a45afe1771f18
ItsazainBilbao <itsazainmbilbao@gmail.com>
2025-06-04 11:56:11 +0200

    api revision

--- api/api_model.py
+++ api/api_model.py
@@ -13,37 +13,39 @@ print(__file__)
 root_path = "/api/api_model.py"
 
 @app.route('/v1/predictCategory', methods=['GET'])
-def predict():
+def predict_category():
     request_text = request.args.get('request_text')
 
-    if None in request_text:
+    if request_text is None:
         return jsonify({"error": "No parameters"}), 400
 
-    catDict = { 0 : "Copy Revision", 1 : "Design Issues", 2 : "Requested Change", 3 : "New Item"}
-    rndNumber = np.random.randint(0,4)
+    catDict = {0: "Copy Revision", 1: "Design Issues", 2: "Requested Change", 3: "New Item"}
+    rndNumber = np.random.randint(0, 4)
 
     prediction = {"Categoría": catDict[rndNumber]}
-    return jsonify({'predictions': prediction[0]})
+    return jsonify({'predictions': prediction})
+
 
 @app.route('/v1/predictPriority', methods=['GET'])
-def predict():
+def predict_priority():
     request_text = request.args.get('request_text')
 
-    if None in request_text:
+    if request_text is None:
         return jsonify({"error": "No parameters"}), 400
 
-    prioDict = { 0 : "Low", 1 : "Medium", 2 : "High", 3: "Urgent"}
-    rndNumber = np.random.randint(0,3)
+    prioDict = {0: "Low", 1: "Medium", 2: "High", 3: "Urgent"}
+    rndNumber = np.random.randint(0, 4)
 
     prediction = {"Prioridad": prioDict[rndNumber]}
-    return jsonify({'predictions': prediction[0]})
+    return jsonify({'predictions': prediction})
 
-@app.route('/v1/predicDuration', methods=['GET'])
-def predict():
+
+@app.route('/v1/predictDuration', methods=['GET'])
+def predict_duration():
     request_text = request.args.get('request_text')
 
-    if None in request_text:
+    if request_text is None:
         return jsonify({"error": "No parameters"}), 400
 
     prediction = {"Duration": np.random.choice([1, 2, 3, 5, 8, 13])}
-    return jsonify({'predictions': prediction[0]})
+    return jsonify({'predictions': prediction})
