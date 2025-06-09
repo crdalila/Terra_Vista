f3ec40ce26d9265cbd53fb26fdad97b899d92e00
Dalila <crdalila@gmail.com>
2025-06-09 15:13:29 +0200

    modal for successfull project

--- client/src/components/taskList/TaskList.jsx
+++ client/src/components/taskList/TaskList.jsx
@@ -9,18 +9,16 @@ import { sendFeedback } from "../../utils/clickup";
 
 function TaskList({ tasks = [], projectId }) {
     const { userData } = useContext(AuthContext);
-    const { selectedProject } = useContext(ProjectContext);
-    const issuesNotSent = tasks.filter(task => task.isSend === "false"); //ARRAY?
-
-    // Show only if it's client
+    const { selectedProject, setSelectedProject } = useContext(ProjectContext);
     const isClient = userData && userData.role === "client";
 
-    const handleFeedback = () => {
+    const handleFeedback = async () => {
         try {
-            sendFeedback(userData._id, issuesNotSent);
+            await sendFeedback(userData._id);
             alert ("Feedback sent successfully");
         } catch (err) {
             console.error("Error sending feedback", err);
+            alert("There was a problem sending the feedback");
         }
     }
 
@@ -29,9 +27,8 @@ function TaskList({ tasks = [], projectId }) {
 
             {isClient && (
                 <section className="tasks-list--buttons">
-
                     <button onClick={handleFeedback} className="button-feedback">Send Feedback</button>
-
+                    
                     <div className="tasks-buttons">
                         <Link to="/request" state={{ project: selectedProject }}>
                             <button className="button-create-task">+ Create Request</button>
@@ -48,7 +45,6 @@ function TaskList({ tasks = [], projectId }) {
                         <TaskCard task={task} key={task._id} projectId={projectId} />)
                 )}
             </section>
-
         </article>
     );
 }
