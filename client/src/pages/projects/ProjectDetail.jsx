fe8f94f5a580746f4978db5df8fd769b96746450
anais <acabado001@ehu.com>
2025-06-10 10:06:46 +0200

    add user to project form changes, users in project changes

--- client/src/pages/projects/ProjectDetail.jsx
+++ client/src/pages/projects/ProjectDetail.jsx
@@ -139,16 +139,18 @@ function ProjectDetail() {
             </section>
 
             <section className="page-content">
-                {usersInProject.map(user => (
-                    <div className="project-user">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="var(--text-color)" width='24' height='24'>
-                            <path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z" />
-                        </svg>
-                        <p key={user._id} value={user._id}>
-                            {user.name} ({user.email})
-                        </p>
-                    </div>
-                ))}
+                <div className="project-users">
+                    {usersInProject.map(user => (
+                        <div className="project-user">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="var(--text-color)" width='24' height='24'>
+                                <path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z" />
+                            </svg>
+                            <p key={user._id} value={user._id}>
+                                {user.name} ({user.email})
+                            </p>
+                        </div>
+                    ))}
+                </div>
 
                 <form className="add-user-to-project-form" onSubmit={handleAddUsersToProject}>
                     <label htmlFor="users">Select clients to add to this project: </label>
