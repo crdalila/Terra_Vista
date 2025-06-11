bb2998bf6439f97210f7440fcdcfd442f066458a
anais <acabado001@ehu.com>
2025-06-11 14:05:24 +0200

    userCard random color

--- client/src/components/userCard/UserCard.jsx
+++ client/src/components/userCard/UserCard.jsx
@@ -11,6 +11,9 @@ function UserCard({ user }) {
     const [userToDelete, setUserToDelete] = useState(null);
     const [error, setError] = useState("");
 
+    const colorList = ['#FFB41D', '#F96E43', '#3D9DD8', '#F78BD8', '#189B5C', '#7CE55E'];
+	const randomColor = colorList[Math.floor(Math.random() * colorList.length)];
+
     const handleRemoveUser = async (userId) => {
         try {
             const result = await userService.removeUser(userId);
@@ -28,7 +31,7 @@ function UserCard({ user }) {
 
     return (
         <article className="user-card-container">
-            <div key={user._id} className="user-card">
+            <div key={user._id} className="user-card" style={{ '--random-color': randomColor }}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="var(--text-color)" width='24' height='24'>
                         <path d="M399 384.2C376.9 345.8 335.4 320 288 320l-64 0c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z" />
                     </svg>
